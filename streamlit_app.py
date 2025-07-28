2a82a4b3dbdc68ba0115e6bbe56bf8389ba20a8f
Leoson Hoay <leosonhoay@gmail.com>
2025-07-28 16:35:36 +0000

    fixed bug with auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
 # Show title and description.
 st.title("💬 Chatbot")
